4a37d4a9acea0e8bfc1276d46353b7f1d01760ed
f.fathollahi <ff.fathollahi@gmail.com>
2021-12-17 11:03:51 +0330

    Add to git

--- ProxyDesignPattern/Program.cs
+++ ProxyDesignPattern/Program.cs
@@ -11,7 +11,7 @@ namespace ProxyDesignPattern
             Console.WriteLine("4+2=" + mathProxy.Add(4,2));
             Console.WriteLine("4*2=" + mathProxy.Mul(4, 2));
             Console.WriteLine("4-2=" + mathProxy.Sub(4, 2));
-            Console.WriteLine("4/2=" + mathProxy.Div(4, 2));
+            Console.WriteLine("4/2=" + mathProxy.Div(4, 2)); 
 
             //
             Console.ReadLine();
