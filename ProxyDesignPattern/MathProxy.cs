2ede1296b8e5ae5b1d94710ccbe857361170da0f
f.fathollahi <ff.fathollahi@gmail.com>
2021-12-17 11:03:24 +0330

    Add project files.

---
+++ ProxyDesignPattern/MathProxy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyDesignPattern
+{
+    public class MathProxy : IMath
+    {
+        private Math math = new Math();
+        public double Add(double x, double y)
+        {
+            return math.Add(x, y);
+        }
+
+        public double Div(double x, double y)
+        {
+            return math.Div(x, y);
+        }
+
+        public double Mul(double x, double y)
+        {
+            return math.Mul(x, y);
+        }
+
+        public double Sub(double x, double y)
+        {
+            return math.Sub(x, y);
+        }
+    }
+}
---
+++ ProxyDesignPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProxyDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathProxy mathProxy = new MathProxy();
+
+            Console.WriteLine("4+2=" + mathProxy.Add(4,2));
+            Console.WriteLine("4*2=" + mathProxy.Mul(4, 2));
+            Console.WriteLine("4-2=" + mathProxy.Sub(4, 2));
+            Console.WriteLine("4/2=" + mathProxy.Div(4, 2));
+
+            //
+            Console.ReadLine();
+        }
+    }
+}
